d8e77ccec3847d166904c4af4312fc48b8cd2fe6
Surinder Jungral <114878952+Surinderpro@users.noreply.github.com>
2024-01-23 21:12:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // Specify the port your application should listen on
-  },
   assetsInclude: ['**/*.glb'],
+  server: {
+    port: process.env.PORT || 5173, // Use the PORT environment variable or default to 5173
+    host: '0.0.0.0',
+  },
 });
