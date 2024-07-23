8c2aae74c7c580f8f9fdf9816b88cb6e5d221870
Surinder Jungral <surinderjungral275@gmail.com>
2024-07-24 01:57:49 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,11 +17,11 @@ const About = () => {
       </h1>
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
-        <p>
-          Web Developer, specializing in technical
-          education through hands-on learning and building applications.
-        </p>
-      </div>
+  <p>
+    DevOps and Cloud Technology Enthusiast | Hands-On Experience with AWS, Docker, GenAIOps, AI, ML, and Linux
+  </p>
+</div>
+
 
       <div className='py-10 flex flex-col'>
         <h3 className='subhead-text'>My Skills</h3>
